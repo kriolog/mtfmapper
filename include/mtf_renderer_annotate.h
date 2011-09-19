5f1cce1ad564a4b4c47dea6ed7a3eb0c3e799efb
fvdbergh <fvdbergh@f0b42d95-50d2-4a92-b73f-df2d32d21376>
2011-09-19 07:09:36 +0000

    fixed up the BSD license text
    git-svn-id: svn://svn.code.sf.net/p/mtfmapper/code/trunk@40 f0b42d95-50d2-4a92-b73f-df2d32d21376

--- include/mtf_renderer_annotate.h
+++ include/mtf_renderer_annotate.h
@@ -23,7 +23,7 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 The views and conclusions contained in the software and documentation are those of the
 authors and should not be interpreted as representing official policies, either expressed
-or implied, of Frans van den Bergh.
+or implied, of the Council for Scientific and Industrial Research (CSIR).
 */
 #ifndef MTF_RENDERER_ANNOTATE_H
 #define MTF_RENDERER_ANNOTATE_H
