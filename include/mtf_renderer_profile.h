cbcbbfa50d3c575c848b88fdb771395e0e254d23
fvdbergh <fvdbergh@f0b42d95-50d2-4a92-b73f-df2d32d21376>
2011-09-23 15:04:00 +0000

    improved handling of the case where the reference edge does not produce a valid mtf50 value
    git-svn-id: svn://svn.code.sf.net/p/mtfmapper/code/trunk@52 f0b42d95-50d2-4a92-b73f-df2d32d21376

--- include/mtf_renderer_profile.h
+++ include/mtf_renderer_profile.h
@@ -127,7 +127,9 @@ class Mtf_renderer_profile : public Mtf_renderer {
         
         FILE* prfile = fopen((wdir+prname).c_str(), "wt");
         i=0;
+        double max_med_filt = 0;
         for (map<int, double>::const_iterator it = row_max.begin(); it != row_max.end(); it++) {
+            max_med_filt = std::max(max_med_filt, med_filt_mtf[i]);
             fprintf(prfile, "%d %lf %lf\n", it->first, it->second, med_filt_mtf[i++]);
         }
         fclose(prfile);
@@ -154,10 +156,16 @@ class Mtf_renderer_profile : public Mtf_renderer {
         }
         
         FILE* pffile = fopen((wdir+pfname).c_str(), "wt");
+        double peak_mtf50 = blocks[largest_block].get_mtf50_value(peak_idx);
+        bool peak_quality_good = true;
+        if (!blocks[largest_block].get_quality(peak_idx)) {
+            peak_mtf50 = max_med_filt;
+            peak_quality_good = false;
+        }
         fprintf(pffile, "%lf %lf %lf\n", 
             blocks[largest_block].get_edge_centroid(peak_idx).y, 
-            blocks[largest_block].get_mtf50_value(peak_idx),
-            blocks[largest_block].get_mtf50_value(peak_idx)*3
+            peak_mtf50,
+            peak_mtf50*3
         );
         fclose(pffile);
 
@@ -167,8 +175,8 @@ class Mtf_renderer_profile : public Mtf_renderer {
         fprintf(gpf, "set ylab \"MTF50 (cyc/pix)\"\n");
         fprintf(gpf, "set term png size 1024, 768\n");
         fprintf(gpf, "set output \"%sprofile_image.png\"\n", wdir.c_str());
-        fprintf(gpf, "plot \"%s\" u 1:2 t \"MTF50 (c/p) raw\" w p ps 0.25, \"%s\" u 1:3 t \"MTF50 (c/p) smoothed\" w l lw 3, \"%s\" u 1:2 t \"Expected focus point\" w i lw 3\n", 
-            (wdir+prname).c_str(), (wdir+prname).c_str(), (wdir+pfname).c_str());
+        fprintf(gpf, "plot \"%s\" u 1:2 t \"MTF50 (c/p) raw\" w p ps 0.25, \"%s\" u 1:3 t \"MTF50 (c/p) smoothed\" w l lw 3, \"%s\" u 1:2 t \"Expected focus point\" w i lc %d lw 3\n", 
+            (wdir+prname).c_str(), (wdir+prname).c_str(), (wdir+pfname).c_str(), peak_quality_good ? 3 : 1);
         fclose(gpf);
         
         char* buffer = new char[1024];
