af7b61b5dc4e034cae784020cf457f9a5de2ecf2
fvdbergh <fvdbergh@f0b42d95-50d2-4a92-b73f-df2d32d21376>
2012-10-10 07:03:48 +0000

    roughly 3.9x speedup in polygon intersection code by avoiding all those mallocs in STL/OpenCV object
    git-svn-id: svn://svn.code.sf.net/p/mtfmapper/code/trunk@117 f0b42d95-50d2-4a92-b73f-df2d32d21376

--- src/generator/render_poly.h
+++ src/generator/render_poly.h
@@ -37,36 +37,24 @@ using namespace cv;
 //==============================================================================
 class Render_rectangle_poly : public Render_rectangle {
   public:
+    static const int MAX_POINTS = 32; // max number of point in intersected polygon
+  
     Render_rectangle_poly(double cx, double cy, double width, double height, double angle) 
     : Render_rectangle(cx, cy, width, height, angle, false) {
+    
     }
     
     virtual ~Render_rectangle_poly(void) {
     }
     
     double evaluate_x(const Render_rectangle& b, double xoffset = 0, double yoffset = 0)  const {
-        double rvalue = compute_area(intersect(b, xoffset, yoffset));
-        return rvalue;
-    }
     
-    // intersection of two rays defined by origin (v1 and v2) and directions (d1 and d2)
-    bool intersect(const Vec2d& v1, const Vec2d& d1, 
-                   const Vec2d& v2, const Vec2d& d2, 
-                   Vec2d& pi) const {
-                   
-        double denom = (d2[1]*d1[0] - d2[0]*d1[1]);
+        double points_x[MAX_POINTS];
+        double points_y[MAX_POINTS];
+        int points_len = 0;
         
-        if (fabs(denom) < 1e-11) {
-           pi[0] = pi[1] = 0;
-           return false;
-        }
-        
-        double u;
-        u = (d2[0]*(v1[1] - v2[1]) - d2[1]*(v1[0] - v2[0])) / denom;
-        
-        pi = v1 + u*d1;
-                   
-        return true;               
+        intersect(points_x, points_y, points_len, b, xoffset, yoffset);
+        return compute_area(points_x, points_y, points_len);
     }
     
     Vec2d t_intersect(const Vec2d& v1, const Vec2d& d1, 
@@ -88,61 +76,114 @@ class Render_rectangle_poly : public Render_rectangle {
         return pi;               
     }
     
-    double compute_area(const vector<Vec2d>& points) const {
+    inline bool t_intersect(double& pix, double& piy, 
+                     const double& v1x, const double& v1y,
+                     const double& d1x, const double& d1y,
+                     const double& v2x, const double& v2y,
+                     const double& d2x, const double& d2y) const {
+                   
+        double denom = (d2y*d1x - d2x*d1y);
+        
+        if (fabs(denom) < 1e-11) {
+           printf("denom zero?\n");
+           return false;
+        }
+        
+        double u = (d2x*(v1y - v2y) - d2y*(v1x - v2x)) / denom;
+        
+        pix = v1x + u*d1x;
+        piy = v1y + u*d1y;
+                   
+        return true;               
+    }
+    
+    double compute_area(double* points_x, double* points_y, int points_len) const {
         double A = 0;
-        int N = (int)points.size();
-        for (int i=0; i < N; i++) {
-            int ni = (i+1) % N;
-            A += points[i][0]*points[ni][1] - points[ni][0]*points[i][1];
+        for (int i=0; i < points_len; i++) {
+            int ni = (i+1) % points_len;
+            A += points_x[i]*points_y[ni] - points_x[ni]*points_y[i];
         }
         return 0.5 * fabs(A);
     }
     
-    vector<Vec2d> intersect(const Render_rectangle& b, double xoffset = 0, double yoffset = 0) const {
-        vector<Vec2d> points;
+    void intersect(double* points_x, double* points_y, int& points_len, 
+        const Render_rectangle& b, double xoffset = 0, double yoffset = 0) const {
         
         for (int i=0; i < 4; i++) {
-            points.push_back(cv::Point_<double>(b.bases[i][0] + xoffset, b.bases[i][1] + yoffset));
+            points_x[i] = b.bases[i][0] + xoffset;
+            points_y[i] = b.bases[i][1] + yoffset;
+            points_len++;
         }
         
         for (int e=0; e < 4; e++) {
-            points = intersect_core(points, e);
+            intersect_core(points_x, points_y, points_len, e);
         }
-        
-        return points;    
     }
     
-    vector<Vec2d> intersect_core(const vector<Vec2d>& inpoints, int e) const {
+    void intersect_core(double* inpoints_x, double* inpoints_y, int& in_len, int e) const {
         int ne = (e + 1) % 4;
         
-        Vec2d P = bases[e];
-        Vec2d D = bases[ne] - bases[e];
+        double Px = bases[e][0];
+        double Py = bases[e][1];
         
-        Vec2d N = Vec2d(-D[1], D[0]);
+        double Dx = bases[ne][0] - Px;
+        double Dy = bases[ne][1] - Py;
+         
+        double Nx = -Dy;
+        double Ny = Dx;
         
-        vector<Vec2d> outpoints;
+        double outpoints_x[MAX_POINTS];
+        double outpoints_y[MAX_POINTS];
+        int out_idx = 0;
         
-        Vec2d S = inpoints[inpoints.size() - 1];
-        Vec2d dS = S - P;
-        for (size_t i=0; i < inpoints.size(); i++) {
-            const Vec2d& E = inpoints[i];
-            Vec2d dE = E - P;
+        double Sx = inpoints_x[in_len - 1];
+        double Sy = inpoints_y[in_len - 1];
+        
+        double dSx = Sx - Px;
+        double dSy = Sy - Py;
+        
+        for (size_t i=0; i < size_t(in_len); i++) {
+            const double& Ex = inpoints_x[i];
+            const double& Ey = inpoints_y[i];
             
-            if (N.dot(dE) >= 0) {
-                if (N.dot(dS) < 0) {
-                    outpoints.push_back( t_intersect(S, E-S, P, D) );
+            double dEx = Ex - Px;
+            double dEy = Ey - Py;
+            
+            if ( (Nx*dEx + Ny*dEy) >= 0) {
+                if ( (Nx*dSx + Ny*dSy) < 0) {
+                    t_intersect(
+                        outpoints_x[out_idx], outpoints_y[out_idx],
+                        Sx, Sy,
+                        Ex - Sx, Ey - Sy,
+                        Px, Py,
+                        Dx, Dy
+                    );
+                    out_idx++;
                 }
-                outpoints.push_back(E);
+                outpoints_x[out_idx] = Ex;
+                outpoints_y[out_idx] = Ey;
+                out_idx++;
             } else {  
-                if (N.dot(dS) >= 0) {
-                    outpoints.push_back( t_intersect(S, E-S, P, D) );
+                if ( (Nx*dSx + Ny*dSy) >= 0) {
+                    t_intersect(
+                        outpoints_x[out_idx], outpoints_y[out_idx],
+                        Sx, Sy,
+                        Ex - Sx, Ey - Sy,
+                        Px, Py,
+                        Dx, Dy
+                    );
+                    out_idx++;
                 }
             }
-            S = E;
-            dS = dE;
+            Sx = Ex;
+            Sy = Ey;
+            dSx = dEx;
+            dSy = dEy;
         }
     
-        return outpoints;
+        in_len = out_idx;
+        memcpy(inpoints_x, outpoints_x, sizeof(double)*out_idx);
+        memcpy(inpoints_y, outpoints_y, sizeof(double)*out_idx);
     }
     
       
