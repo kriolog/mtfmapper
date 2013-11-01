74fe5769b59e2d3b994e0f2b22d8b267e0744a6a
fvdbergh <fvdbergh@f0b42d95-50d2-4a92-b73f-df2d32d21376>
2013-11-01 10:44:56 +0000

    doh! two rights make a wrong
    git-svn-id: svn://svn.code.sf.net/p/mtfmapper/code/trunk@140 f0b42d95-50d2-4a92-b73f-df2d32d21376

--- src/generator/render_is_airyolpf.h
+++ src/generator/render_is_airyolpf.h
@@ -87,7 +87,7 @@ class Render_polygon_is_airyolpf : public Render_polygon_is {
 		double olpf_y[4] = {-olpf_split,  olpf_split, -olpf_split, olpf_split};
 		        
         for (int k=0; k < 4; k++) {
-            double area = photosite.intersection_area(target, ex + x + olpf_x[k], ey + y + olpf_y[k]) / photosite.own_area;
+            double area = target.intersection_area(photosite, ex + x + olpf_x[k], ey + y + olpf_y[k]) / photosite.own_area;
             sample += 0.25*(object_value * area + background_value * (1 - area));
         }
                 
